b26146a0b62615faed188801468b06ea9b0ddf35
spider10584329 <spider10584329@gmail.com>
2025-10-02 18:44:05 +0200

    frist deploy2

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = 'https://flrfnklpswvbnzfjvwau.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZscmZua2xwc3d2Ym56Zmp2d2F1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzODc3MzYsImV4cCI6MjA3NDk2MzczNn0.Nn0TQHPBr5vRnsuypcU7BdZxsxFmQPJ7qBOiu6mKeO0'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
